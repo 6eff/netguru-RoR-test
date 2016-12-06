6f953899ccbad85692fb53dbee5ec844eac63f51
6eff <6eff@mail.ru>
2016-12-06 17:02:45 +0300

    Decent exposuer API changed
    Updated app controller respectively

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module RorWorkshops
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.web_console.development_only = false
   end
 end
