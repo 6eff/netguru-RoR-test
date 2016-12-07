f54b20e8ee343eb881a79ff9cf3e976aeeaf79d8
6eff <6eff@mail.ru>
2016-12-07 16:51:34 +0300

    Changes app controller
    devise API update, diff syntax'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "devise/sessions#new"
   resources :students do
     get :subjects
   end
--- db/migrate/20161207100051_add_subject_item_to_subject_item_notes.rb
+++
@@ -1,5 +0,0 @@
-class AddSubjectItemToSubjectItemNotes < ActiveRecord::Migration
-  def change
-    add_column :subject_item_notes, :subject_item, :string
-  end
-end
