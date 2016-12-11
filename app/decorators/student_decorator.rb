254f7dea1ac00411bf1ee7e49ebdb06fc2aa6a3d
6eff <6eff@mail.ru>
2016-12-11 21:27:40 +0300

    Adds birthdate to reports

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -6,4 +6,8 @@ class StudentDecorator < BaseDecorator
   def avg_notes(subject_item)
     '%.2f' % "#{subject_item.subject_item_notes.average(:value) || 0.00}"
   end
+
+  def birth_date
+    birthdate.strftime('%Y-%m_%d') if birthdate
+  end
 end
